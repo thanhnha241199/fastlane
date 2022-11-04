5065f1f0aae04c99c58c7952b464f1e046b79181
Nhã Huỳnh <huynhthanhnha24111999@gmail.com>
2022-11-04 09:56:38 +0700

    update

--- build/app/generated/source/buildConfig/dev/debug/com/example/flutter_boiler/BuildConfig.java
+++ build/app/generated/source/buildConfig/dev/debug/com/example/flutter_boiler/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.example.flutter_boiler.dev.dev";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "dev";
-  public static final int VERSION_CODE = 10;
+  public static final int VERSION_CODE = 16;
   public static final String VERSION_NAME = "1.0.0";
 }
---
+++ build/app/generated/source/buildConfig/prod/release/com/example/flutter_boiler/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.flutter_boiler;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.flutter_boiler.dev";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "prod";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "1.0.0";
+}
